d0ee8c7526105f2ae1cc7f8ae6707a0b8868257f
Nathan Ngo <nathanngo@Nathans-MacBook-Pro.local>
2022-04-20 23:37:25 +0700

    self-learning-SQL

--- 5.SQL-Fundamental/Self-Learning/HackerRank-SQL/Problem-12/solution.sql
+++ 5.SQL-Fundamental/Self-Learning/HackerRank-SQL/Problem-12/solution.sql
@@ -8,17 +8,19 @@ GROUP BY h.hacker_id, h.name
 -- If more than one student created the same number of challenges and the count is less than the maximum 
 -- number of challenges created, then exclude those students from the result. 
 HAVING count_challenges = (SELECT MAX(vt_1.count_challenges)
-                            FROM(
-                                SELECT COUNT(*) AS count_challenges
-                                FROM Challenges_1
-                                GROUP BY hacker_id
-                                ) AS vt_1)
+     FROM(
+          SELECT COUNT(*) AS count_challenges
+          FROM Challenges_1
+          GROUP BY hacker_id
+          ) AS vt_1
+     )
 OR count_challenges IN (SELECT vt_1.count_challenges 
-                          FROM(
-                              SELECT COUNT(*) AS count_challenges
-                              FROM Challenges_1
-                              GROUP BY hacker_id
-                              ) AS vt_1
-                              GROUP BY count_challenges
-                              HAVING COUNT(*) = 1) -- it is vt_2
+     FROM(
+          SELECT COUNT(*) AS count_challenges
+          FROM Challenges_1
+          GROUP BY hacker_id
+          ) AS vt_1
+     GROUP BY count_challenges
+     HAVING COUNT(*) = 1
+     ) -- it is vt_2
 ORDER BY count_challenges DESC, h.hacker_id
