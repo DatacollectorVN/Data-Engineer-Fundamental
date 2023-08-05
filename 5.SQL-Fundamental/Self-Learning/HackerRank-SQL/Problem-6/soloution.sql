4bc086b356e9007803e12a2473a395a43f21461c
Nathan Ngo <86464633+DatacollectorVN@users.noreply.github.com>
2023-08-05 23:18:19 +0700

    Update soloution.sql

--- 5.SQL-Fundamental/Self-Learning/HackerRank-SQL/Problem-6/soloution.sql
+++ 5.SQL-Fundamental/Self-Learning/HackerRank-SQL/Problem-6/soloution.sql
@@ -9,6 +9,6 @@ WHERE S1.rowindex IN (FLOOR(@rowindex / 2) , CEIL(@rowindex / 2));
 -- REMEMBER: The value of @rowindex in WHERE clause = (length of column - 1). Mean it is last update of @rowindex.
 /*
 i.e: 
-+ length of column = 5 --> @rowindex = 4 --> FLOOR(@rowindex / 2) = 2 and CEIL(@rowindex / 2) = 2
-+ length of column = 6 --> @rowindex = 5 --> FLOOR(@rowindex / 2) = 2 and CEIL(@rowindex / 2) = 3
++ length of data = 5 --> @rowindex = 4 --> FLOOR(@rowindex / 2) = 2 and CEIL(@rowindex / 2) = 2
++ length of data = 6 --> @rowindex = 5 --> FLOOR(@rowindex / 2) = 2 and CEIL(@rowindex / 2) = 3
 */
